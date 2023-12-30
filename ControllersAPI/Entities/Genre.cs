d517304d13d970d220e372869a888da502fb3df0
César Zárate <cesarjzo@outlook.com>
2023-12-30 16:15:30 -0600

    Add IValidatableObject interface to Genre class for name validation

--- ControllersAPI/Entities/Genre.cs
+++ ControllersAPI/Entities/Genre.cs
@@ -6,12 +6,20 @@ namespace ControllersAPI.Entities;
 /// <summary>
 /// Represents a genre of a movie.
 /// </summary>
-public sealed record Genre
+public sealed record Genre : IValidatableObject
 {
     public int Id { get; init; }
 
     [Required(ErrorMessage = "The '{0}' field is required!")]
     [StringLength(10)]
-    [FirstLetterUppercase]
+    // [FirstLetterUppercase]
     public string Name { get; set; } = string.Empty;
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        ReadOnlySpan<char> span = Name;
+
+        if (!char.IsUpper(span[0]))
+            yield return new ValidationResult("First letter must be uppercase.", new[] { nameof(Name) });
+    }
 }
