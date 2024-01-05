fc25dc5f3e64e0444caedb9b042be6ebd8d3dc75
César Zárate <cesarjzo@outlook.com>
2024-01-05 16:58:31 -0600

    Add logging and pagination to ActorsController and Get and Delete

--- ControllersAPI/Controllers/ActorsController.cs
+++ ControllersAPI/Controllers/ActorsController.cs
@@ -10,6 +10,7 @@ namespace ControllersAPI.Controllers;
 [Route("api/actors")]
 public sealed class ActorsController(
     ApplicationDbContext dbContext,
+    ILogger<Actor> logger,
     IMapper mapper,
     IFileStorage fileStorage
 ) : ControllerBase
@@ -17,10 +18,24 @@ public sealed class ActorsController(
     private const string ContainerName = "actors";
 
     private readonly ApplicationDbContext _dbContext = dbContext;
-    
+
+    private readonly ILogger<Actor> _logger = logger;
+
     private readonly IMapper _mapper = mapper;
 
     private readonly IFileStorage _fileStorage = fileStorage;
+
+    [HttpGet]
+    public async Task<ActionResult<ActorDTO[]>> Get([FromQuery] PaginationDTO pagination)
+    {
+        IQueryable<Actor> queriable = _dbContext.Actors.AsQueryable();
+
+        await HttpContext.InsertPaginationParamsInHeader(queriable);
+
+        var actors = queriable.OrderBy(a => a.Name).Paginate(pagination);
+
+        return _mapper.Map<ActorDTO[]>(actors);
+    }
 
     [HttpPost]
     public async Task<ActionResult> Post([FromForm] ActorCreationDTO actorCreationDTO)
@@ -30,8 +45,35 @@ public sealed class ActorsController(
         if (actorCreationDTO.Picture is not null)
             actor.Photo = await _fileStorage.SaveFile(ContainerName, actorCreationDTO.Picture);
 
+        _logger.LogInformation("Creating actor {name}.", actor.Name);
+
         _dbContext.Add(actor);
         await _dbContext.SaveChangesAsync();
+        return NoContent();
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<ActionResult> Delete(int id)
+    {
+        Actor? actor = await _dbContext.Actors.FindAsync(id);
+
+        if (actor is null)
+        {
+            _logger.LogWarning("Actor with id {id} not found.", id);
+            return NotFound();
+        }
+
+        _logger.LogInformation("Deleting actor {name} with id {id}.", actor.Name, id);
+
+        _dbContext.Remove(actor);
+        await _dbContext.SaveChangesAsync();
+
+        if (actor.Photo is not null)
+        {
+            _logger.LogInformation("Deleting actor {name} photo.", actor.Name);
+            await _fileStorage.DeleteFile(actor.Photo, ContainerName);
+        }
+
         return NoContent();
     }
 }
